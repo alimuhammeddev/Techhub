3999783b7aa4dcf82fb1180febe7e9a750ddb4e7
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-05-31 13:41:59 +0100

    worked on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
               <img src={logo} alt="" />
             </div>
             
-            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-black'>
+            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer lg:hidden text-black'>
             <ion-icon name={open ? 'close':'menu'}></ion-icon>
             </div>
 

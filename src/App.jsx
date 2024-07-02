c15fee41946e9b0f761ccfec6593bcef1ea725b1
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-07-02 01:01:04 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home'
 import About from './Pages/About';
 import Faqs from './Pages/Faqs';
+import ScrollToTop from './Components/ScrollToTop';
 
 const App = () => {
   return (
     <Router>
   <div>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
